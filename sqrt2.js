0861e576071aed1f4d43f3d62dce103f02fc32fd
iryanb <32988577+iryanbinary@users.noreply.github.com>
2017-12-20 09:18:15 -0800

    Create sqrt2.js
    javascript math to compute the square root of 2 as a way to think about how the Pythagoreans and Geomori solved the problem of incommensurability in triangles such as an equilateral triangle a = 1, b = 1, and c = Square Root of 2

---
+++ sqrt2.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', function(e) {
+  document.querySelector('#SqRt2').innerHTML = 'incommensurability created by Ryan B ( @iryanb ) on 12.20.2017 © ';
+}, false);
+
+var a = 1;
+var b = 1;
+var n = 1;
+var c = 2;
+var d = 3;
+var start = Date.now();
+document.writeln("<br /> if a = 1 & b = 1 & c = a+b & d = 2a+b then the Square Root of 2 = d / c as it is close to 1.414213562373095 after 20 iterations until reaching 32 bit infinity of scientific notation floating point precision after 806 iterations" + "<br />");
+
+for(n = 1; n < 807; n++){
+c=a+b;
+d=(2 * a) + b;
+sqrt=d/c;
+
+document.writeln(" <br />"); document.writeln("for iteration " + n + " c = " + c + " d = " + d + " SqRt2 is " + sqrt);
+a=c;
+b=d;
+
+
+document.writeln(" <br />");
+}
+var speed = "average";
+t=Math.abs(Date.now() - start);
+if (t < 24) { speed = "fast";}
+if (t > 50) { speed = "slow";}
+document.writeln(" <br />");
+document.writeln("Compute Time = " + t + " (ms) . " + speed);
+
+
+
+
